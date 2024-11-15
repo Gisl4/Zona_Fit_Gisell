3fdf8f95b2b800269a0f1c85a3ef1e752e85d1c8
Gisl4 <lopgiz25@gmail.com>
2024-11-15 15:12:57 +0100

    ZonaFit_Consola_Escritorio_Web

---
+++ src/main/java/gl/zona_fit/servicio/IClienteServicio.java
@@ -0,0 +1,14 @@
+package gl.zona_fit.servicio;
+
+import gl.zona_fit.modelo.Cliente;
+import java.util.List;
+
+public interface IClienteServicio {
+    public List<Cliente> listarClientes();
+
+    public Cliente buscarClientePorId(Integer idCliente);
+
+    public void guardarCliente(Cliente cliente);
+
+    public void eliminarCliente(Cliente cliente);
+}
